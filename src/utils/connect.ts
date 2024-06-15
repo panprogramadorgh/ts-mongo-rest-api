6ce86e9c46d7af910294b6b9e89b695242779bd8
panprogramadorgh <barreroalvaro2007@gmail.com>
2024-06-15 20:53:07 +0200

    docker development

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -2,13 +2,13 @@ import { connect as connectDB } from 'mongoose'
 import readEnv from './read-env'
 
 export default async function connect() {
-  const dbURI = readEnv("DB_URI")
-  if (!dbURI) {
-    throw new Error("DB_URI env variable must be defined")
+  const dbURL = readEnv("DB_URL")
+  if (!dbURL) {
+    throw new Error("DB_URL env variable must be defined")
   }
 
   try {
-    await connectDB(dbURI!)
+    await connectDB(dbURL!)
     console.log("connected to database")
   } catch (error) {
     console.log("cannot connecto to database")
